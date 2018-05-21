84b9005995fb808e045948cb490a91a2941571c8
yifei <yifei@feideMacBook-Pro.local>
2018-05-21 22:40:02 +0800

    [feat] ui optimize

--- test-module1/src/main/java/com/sjtu/yifei/test_module1/FragmentActivity.java
+++ test-module1/src/main/java/com/sjtu/yifei/test_module1/FragmentActivity.java
@@ -3,6 +3,7 @@ package com.sjtu.yifei.test_module1;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.sjtu.yifei.annotation.Route;
 import com.sjtu.yifei.route.RouteImpl;
@@ -14,6 +15,8 @@ public class FragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Fragment fragment = RouteImpl.getTestFragment("from FragmentActivity", "xxxxaaaaa");
         getSupportFragmentManager()
